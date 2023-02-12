016f07f4d15376277d12c0f699809db737458cc9
iHarryD <anuraagvr555@gmail.com>
2023-02-12 14:59:34 +0530

    style: create global and variables stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/global.css";
+import "./styles/variables.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
